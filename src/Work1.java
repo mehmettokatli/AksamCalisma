190e375e6d207a5949f3c968be76b7492873b73c
mehme <mehmet.tokatli999@gmail.com>
2022-04-23 22:03:00 +0300

    Sorun2

--- src/Work1.java
+++ src/Work1.java
@@ -2,7 +2,8 @@ public class Work1 {
     public static void main(String[] args) {
         System.out.println("Başlangıç ");
         System.out.println( "ikinci metin");
-        System.out.println( "Halilcim, collaborator seçtikten sonra ona mesaj yazıyor muyduk. onu bulamadım");
 
+        System.out.println( "Halilcim, collaborator seçtikten sonra ona mesaj yazıyor muyduk. onu bulamadım");
+        //bir diğer sorun şu: collaborator bir şeyler yaptı ve bana gönderdi. ben bundan nasıl haber alıyordum?
     }
 }
