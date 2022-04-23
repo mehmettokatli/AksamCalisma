c94db97272d6e65066d43f4b73f5e93b89205d03
mehme <mehmet.tokatli999@gmail.com>
2022-04-23 21:56:48 +0300

    Sorun

--- src/Work1.java
+++ src/Work1.java
@@ -2,5 +2,7 @@ public class Work1 {
     public static void main(String[] args) {
         System.out.println("Başlangıç ");
         System.out.println( "ikinci metin");
+        System.out.println( "Halilcim, collaborator seçtikten sonra ona mesaj yazıyor muyduk. onu bulamadım");
+
     }
 }
